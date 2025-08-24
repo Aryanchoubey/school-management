73f070b7d3b3c91cd6544e53a16488d1ac1d9f78
BEaANIKET <aniketchaturvedi309@gmail.com>
2025-08-24 11:43:34 +0530

    second

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
      <h1 className="bg-amber-500 text-4xl text-red-500"> Hello </h1>
+     <Button variant="secondary" > Hello </Button>
     </>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths' // handles @ alias
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
+    tsconfigPaths(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {

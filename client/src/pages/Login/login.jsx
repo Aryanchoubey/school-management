2bda901d18b6c2a938e1cdd16fcc5acb7defaad4
BEaANIKET <aniketchaturvedi309@gmail.com>
2025-08-26 14:11:25 +0530

    first

--- client/src/pages/Login/login.jsx
+++ client/src/pages/Login/login.jsx
@@ -8,7 +8,6 @@ const Login = () => {
           <img className=" h-full object-cover" src="https://imgs.search.brave.com/tN6wno47SQgjxHPhOQ4A4J3OL0hjyrKdov5q7s4L9FA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8x/Mi8wMS8wNC80Mi9t/YW4tNzYyODMwNV82/NDAuanBn" alt="" />
         </div>
         <div className=" min-w-[500px] flex justify-center items-center ">
-
         </div>
       </div>
     </>
